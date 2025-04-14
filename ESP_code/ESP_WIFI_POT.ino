45a2e73b7a9e415097c2fe19b3f209d78b6ab8a7
Kobe <87835153+KbeDemy@users.noreply.github.com>
2025-04-14 17:47:49 +0200

    Update ESP_WIFI_POT.ino
    + wifimanager 
    + NEOpixel

--- ESP_code/ESP_WIFI_POT.ino
+++ ESP_code/ESP_WIFI_POT.ino
@@ -1,36 +1,45 @@
 #include <WiFi.h>
+#include <WiFiManager.h> 
 #include <HTTPClient.h>
-#include "Adafruit_SHT4x.h"
+#include "Adafruit_SHT4x.h" // tempsensor
+#include <Adafruit_NeoPixel.h> 
 
-Adafruit_SHT4x sht4 = Adafruit_SHT4x();
-const char* ssid = "*****"; // 
-const char* password = "*****"; // 
 const char* serverName = "http://192.168.0.242:5000/update";  // Controleer dit IP!
 
 const uint8_t potentiometerPin = 32; // GPIO ...
 const uint8_t onPin = 13; 
+const uint8_t debugLed = 0;
+
+Adafruit_SHT4x sht4 = Adafruit_SHT4x();
+Adafruit_NeoPixel pixels(1, debugLed, NEO_GRB + NEO_KHZ800); // 1 = aantal leds
+
 void setup() {
   Serial.begin(115200);
-  WiFi.begin(ssid, password);
+  pixels.begin(); 
 
- 
   pinMode(onPin, OUTPUT);
   digitalWrite(onPin, HIGH);
 
+  WiFiManager wifiManager;
+  wifiManager.resetSettings(); // UNCOMMENT FOR TESTING!
+
+  changeNeoColor(100,0,100);
+
+  if (!wifiManager.autoConnect("smartbrace_SETUP")) {
+    changeNeoColor(100,100,0);
+    Serial.println("Failed to connect and hit timeout");
+    ESP.restart();
+  }
+
+  Serial.println("Verbonden met WiFi!");
+ 
   if (! sht4.begin()) {
     Serial.println("Couldn't find SHT4x");
     while (1) delay(1);
   }
-  
+
   sht4.setPrecision(SHT4X_HIGH_PRECISION);
   sht4.setHeater(SHT4X_NO_HEATER);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Verbindt met WiFi...");
-  }
-
-  Serial.println("Verbonden met WiFi!");
 }
 
 void loop() {
@@ -38,7 +47,7 @@ void loop() {
   sht4.getEvent(&humidity, &temp);
 
   int potentiometerValue = analogRead(potentiometerPin);
-
+ 
   int Angle = map(potentiometerValue, 0, 4095, 0, 180);
 
   if (WiFi.status() == WL_CONNECTED) {
@@ -56,15 +65,23 @@ void loop() {
 
     if (httpResponseCode > 0) {
       Serial.println("Server Responded!");
+       changeNeoColor(0,100,0);
     } else {
       Serial.print("Fout bij versturen! HTTP-code: ");
       Serial.println(httpResponseCode);
+      changeNeoColor(100,0,0);
     }
 
     http.end();
   } else {
     Serial.println("WiFi niet verbonden");
+    changeNeoColor(100,100,0);
   }
 
-  delay(500);  // Wacht 5 seconden voor de volgende meting
+  delay(500);  
+}
+
+void changeNeoColor(uint8_t red, uint8_t green, uint8_t bleu){
+  pixels.setPixelColor(0, pixels.Color(red, green, bleu));
+  pixels.show(); 
 }
